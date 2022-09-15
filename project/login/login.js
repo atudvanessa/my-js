fca4f2f317ae673f05d1ca1c12f4aee4dfbae6aa
unknown <atudagum1@gmail.com>
2022-09-15 22:14:01 +0100

    edited the js file

--- project/login/login.js
+++ project/login/login.js
@@ -1,13 +1,11 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const email = loginForm.email.value;
-    const password = loginForm.password.value;
+const login = document.getElementById("login")
+button.addEventListener("click",(e) => {
+    const email = login.email.value;
+    const password = Form.password.value;
 
-    if (email === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
+    if (email ==="user" && password === "alpha"){
+        alert("your email is user password is alpha")
         location.reload();
     }
 })
