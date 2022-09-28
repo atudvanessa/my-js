4ca9db816ce135333f832c0358e2062f83937e59
unknown <atudagum1@gmail.com>
2022-09-28 22:11:29 +0100

    added a README.md

--- project/login/login.js
+++ project/login/login.js
@@ -1,8 +1,9 @@
 
 const login = document.getElementById("login")
-button.addEventListener("click",(e) => {
+const Button = document.getElementById("Button");
+Button.addEventListener("click",(e) => {
     const email = login.email.value;
-    const password = Form.password.value;
+    const password = form.password.value;
 
     if (email ==="user" && password === "alpha"){
         alert("your email is user password is alpha")
