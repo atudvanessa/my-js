e9fed1bac2cbfafe842aa6795f73978094f522dc
unknown <atudagum1@gmail.com>
2022-10-05 19:34:18 +0100

    chapter5

--- exercise/chapter5/chapter5exe.js
+++ exercise/chapter5/chapter5exe.js
@@ -1,1 +1,18 @@
-//
+//flattening
+
+const arrays = [[7, 2, 3], [9, [4, 5], 8], [6]];
+const flattened = arrays.reduce(
+    function(current,nextValue) {
+        return current.concat(nextValue);
+    }
+)
+console.log(flattened);
+
+
+// your own loop
+function loop(value, test, update, execute) {
+    if (!test(value)) return; // stop
+    execute(value);
+    return loop(update(value), test, update, execute);
+  }
+  loop(3, n => n > 0, n => n - 1, console.log);
